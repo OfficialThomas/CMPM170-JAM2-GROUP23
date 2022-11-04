8f6b0cff72736ecbd556e94cd502a8a275f86333
domberardi10 <domberardi10@gmail.com>
2022-11-03 19:41:59 -0700

    Scene transition to platformer scene

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -6,11 +6,24 @@ using UnityEngine.SceneManagement;
 public class SceneTransition : MonoBehaviour
 {
     public string sceneName;
+    public CameraManager fadeControl;
+    private bool isCollided = false;
+    public float timer = 2f;
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            SceneManager.LoadScene(sceneName);
+            fadeControl.FadeOut();
+            isCollided = true;
+        }
+    }
+
+    void Update(){
+        if (isCollided){
+            timer -= Time.deltaTime;
+            if (timer < 0){
+                SceneManager.LoadScene(sceneName);
+            }
         }
     }
 
